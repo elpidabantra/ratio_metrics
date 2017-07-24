adf56c40247186b529dbd2536b57de0b2a2a4f2a
Madhuragk <madhurakashikar2393@gmail.com>
2017-07-24 22:03:21 +0100

    This is the file with error because of nonetype

--- diff.py
+++ diff.py
@@ -6,10 +6,16 @@ Created on Thu Jul 13 23:00:49 2017
 """
 
 import pygit2
+import re
+import string
 #import subprocess
 
 
-repo = pygit2.Repository('C:/Users/Madhura Kashikar/Desktop/Trial/tacotron/')
+
+
+
+
+repo = pygit2.Repository('C:/Users/Madhura Kashikar/Desktop/Trial/Python')
 print(repo.path)
 print(repo.workdir)
 
@@ -19,29 +25,39 @@ commit = repo[repo.head.target]
 name_a=[]
 commit_list=[]
 last = repo[repo.head.target]
-
-
+emaila=[]
+email_u=[]
+data=[]
+email=[]
+cid=[]
+author=[]
 
 
 for commit in repo.walk(last.id, pygit2.GIT_SORT_TIME):
     
     name_a.append(commit.author.name)
-    #print(name_a)
+    emaila.append(commit.author.email)
     name_a = list(set(name_a))
     commit_list.append(commit.tree_id.hex)
-
-num=len(name_a)
-
-
-num1=len(commit_list)
-
-for x, y in zip(commit_list, commit_list[1:]):
-    diff=repo.diff(x,y)
-    patches=[p for p in diff]
-    print("\n \t",patches)
-    print("\n \t",diff.patch)
-    DiffStats.insertions(diff)
+    email_u=list(set(emaila))
     
-    #k=diff._iter_()
-    #diff._len_
+
+
+
+# for x, y in zip(commit_list, commit_list[1:]):
+#     diff=repo.diff(x,y)
+#     patches=[p for p in diff]
+#     print("\n \t",patches)
+#     print("\n \t",diff.patch)
+
+    
+for x in commit_list:
+    diff=repo.diff(x)
+    textdata=diff.patch #this line gives us patch of the code. We need to parse this textData.
+    print("\n the data type of diff is:", type(textdata))
+
+
+   
+  
+    
     
